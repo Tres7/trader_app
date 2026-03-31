f9f422f9a55e901caa8a6b1e0e0c63a0269d19a8
Godwin <kpoglo.godwin10@gmail.com>
2026-03-31 20:01:48 +0200

    delete semi-colon after interface declaratioons

--- client/src/features/auth/model/types.ts
+++ client/src/features/auth/model/types.ts
@@ -1,12 +1,12 @@
 export interface LoginPayload {
-  email: string;
-  password: string;
+  email: string,
+  password: string
 };
 
 export interface LoginApiResponse {
-  accessToken: string;
-  tokenType: string;
-  userId: string;
-  email: string;
-  firstName: string;
+  accessToken: string,
+  tokenType: string,
+  userId: string,
+  email: string,
+  firstName: string
 };
