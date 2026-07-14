183802daa27937645b94c441d7e4f5a5e5f48002
Dzidoula Godwin KPOGLO <kpoglo.godwin10@gmail.com>
2026-07-14 19:04:29 +0200

    ci: push manifest updates via a scoped GitHub App instead of an open bypass

--- server/src/main/java/com/traderapp/TraderappApplication.java
+++ server/src/main/java/com/traderapp/TraderappApplication.java
@@ -3,7 +3,6 @@ package com.traderapp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-// Test commit to trigger the build-publish pipeline.
 @SpringBootApplication
 public class TraderappApplication {
 
